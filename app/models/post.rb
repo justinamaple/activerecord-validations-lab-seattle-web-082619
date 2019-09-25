a9068e89fde16969ad98f07ef213fe7d2fe8e099
Justin Maple <justinamaple@gmail.com>
2019-09-25 23:09:36 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  valides :content, length: { minimum: 250 }
-  valides :summary, length: { maximum: 250 }
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
