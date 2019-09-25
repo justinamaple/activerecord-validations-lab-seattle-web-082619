a0384ed22acf31c7ed5c1a1f80ef3a601fc995df
Justin Maple <justinamaple@gmail.com>
2019-09-25 23:09:22 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   valides :content, length: { minimum: 250 }
   valides :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
