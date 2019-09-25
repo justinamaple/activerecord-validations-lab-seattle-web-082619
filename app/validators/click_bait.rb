884669e4d3a1796fdbff5bbd797f761a833c7256
Justin Maple <justinamaple@gmail.com>
2019-09-25 23:11:16 +0000

    Automatically backed up by Learn

---
+++ app/validators/click_bait.rb
@@ -0,0 +1,7 @@
+class EmailValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.email.match?(/flatironschool.com/)
+      record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
+    end
+  end
+end
