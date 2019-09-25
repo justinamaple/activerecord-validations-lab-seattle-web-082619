40d8a96bcaf951aa3de777c019a4fb61b1fb7700
Justin Maple <justinamaple@gmail.com>
2019-09-25 23:17:58 +0000

    Automatically backed up by Learn

--- app/validators/click_bait_validator.rb
+++ app/validators/click_bait_validator.rb
@@ -1,6 +1,6 @@
 class ClickBaitValidator < ActiveModel::Validator
   def validate(record)
-    unless record.title.include?(/Won't Believe|Secret|Top[1-9]|Guess/)
+    unless record.title.match(/Won't Believe|Secret|Top[1-9]|Guess/)
       record.errors[:title] << "Not clickbait-y enough"
     end
   end
