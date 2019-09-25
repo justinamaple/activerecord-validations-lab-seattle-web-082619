28637edb2a8d4bd476b5fefc43cb1e5f921a2de2
Justin Maple <justinamaple@gmail.com>
2019-09-25 23:14:05 +0000

    Automatically backed up by Learn

--- app/validators/click_bait.rb
+++ app/validators/click_bait.rb
@@ -1,6 +1,6 @@
 class ClickBaitValidator < ActiveModel::Validator
   def validate(record)
-    unless record.title.include?(/Won't Believe|Secret|Top[/)
+    unless record.title.include?(/Won't Believe|Secret|Top[1-9]/)
       record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
     end
   end
