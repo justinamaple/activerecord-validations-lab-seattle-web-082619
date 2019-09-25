319a2fe80b65f957a0e54281ea6a99ed2807fb99
Justin Maple <justinamaple@gmail.com>
2019-09-25 23:08:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :name, uniqueness
   valides :content, length: { minimum: 250 }
   valides :summary, length: { maximum: 250 }
 end
